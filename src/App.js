91bf8e2f8ce268e03be90a062ad467a9b251e913
jdmedlock <jdmedlock@gmail.com>
2018-09-04 19:19:56 -0500

    Refactor: Add ReactRouter and componentDidMount to retrieve books
    Add ReactRouter <BrowserRouter> to `index.js`
    Add `componentDidMount` to App.js to retrieve the books into the state
    
    Resolves: N/a
    See also: #3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react'
-// import * as BooksAPI from './BooksAPI'
-import './App.css'
+import React from 'react';
+import * as BooksAPI from './BooksAPI';
+import './App.css';
 
 class BooksApp extends React.Component {
   state = {
@@ -10,7 +10,17 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: false,
+    books: []
+  }
+
+  /**
+   * Start by retrieving all books and adding them to the state
+   */
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books });
+    })
   }
 
   render() {
