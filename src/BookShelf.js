aa706506f51cc9eff7404d4cb87ad5fa0b0153b9
jdmedlock <jdmedlock@gmail.com>
2018-09-09 10:13:52 -0500

    Refactor: Add book count to bookshelf title
    Add book count to bookshelf title
    
    Resolves: N/a
    See also: #15

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -22,7 +22,7 @@ class BookShelf extends Component {
 
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">{title}</h2>
+        <h2 className="bookshelf-title">{title} ({books.length})</h2>
         <div className="bookshelf-books">
           {books.length === 0 ? (
             <p>No books have been assigned to this shelf</p>
