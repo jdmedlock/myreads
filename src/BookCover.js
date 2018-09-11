680137cf3696c769d3a430cce5d67b931fec488b
jdmedlock <jdmedlock@gmail.com>
2018-09-10 19:35:37 -0500

    Fix: Correct issues and suggestions from Udacity code review
    Correct issues and suggestions from Udacity code review
    - Implement input search text debounce of 100ms
    - Change stateful components to stateless components where applicable
    
    Resolves: N/a
    See also: N/a

--- src/BookCover.js
+++ src/BookCover.js
@@ -1,44 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import BookShelfChanger from './BookShelfChanger';
 
-class BookCover extends Component {
-
-  static propTypes = {
-    book: PropTypes.object.isRequired,
-    changeShelf: PropTypes.func.isRequired,
-    shelfNames: PropTypes.array.isRequired
-  }
+const BookCover = (props) => {
 
   /**
    * @description Create the main page containing the book cover
    * @returns {HTMLDivElement} Book cover
    * @memberof BookCover
    */
-  render() {
-    const { imageLinks, title, authors } = this.props.book;
+  const { imageLinks, title, authors } = props.book;
 
-    return (
-      <div className="book">
-        <div className="book-top">
-          { imageLinks !== undefined && (
-            <div className="book-cover"
-              style={{ width: 128, height: 193,
-              backgroundImage: `url(${imageLinks.thumbnail})` }}>
-            </div>
-          )}
-          <BookShelfChanger book={this.props.book}
-            changeShelf={this.props.changeShelf}
-            shelfNames={this.props.shelfNames}
-          />
-        </div>
-        <div className="book-title">{title}</div>
-        {authors !== undefined && authors.length > 0 && (
-          <div className="book-authors">{authors[0]}</div>
+  return (
+    <div className="book">
+      <div className="book-top">
+        { imageLinks !== undefined && (
+          <div className="book-cover"
+            style={{ width: 128, height: 193,
+            backgroundImage: `url(${imageLinks.thumbnail})` }}>
+          </div>
         )}
+        <BookShelfChanger book={props.book}
+          changeShelf={props.changeShelf}
+          shelfNames={props.shelfNames}
+        />
       </div>
-    );
-  }
+      <div className="book-title">{title}</div>
+      {authors !== undefined && authors.length > 0 && (
+        <div className="book-authors">{authors[0]}</div>
+      )}
+    </div>
+  );
+}
+
+BookCover.propTypes = {
+  book: PropTypes.object.isRequired,
+  changeShelf: PropTypes.func.isRequired,
+  shelfNames: PropTypes.array.isRequired
 }
 
 export default BookCover;
